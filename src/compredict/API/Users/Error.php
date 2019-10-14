073373ff690fba51a1def43c5fb4cb78f6530907
COMPEDICT <ghattas@compredict.de>
2019-10-14 17:15:20 +0200

    update errors

--- src/compredict/API/Users/Error.php
+++ src/compredict/API/Users/Error.php
@@ -8,7 +8,7 @@ class Error extends \Exception
 {
     public function __construct($response, $code)
     {
-        $message = (isset($response->errors)) ? $response->errors[0] : $response->error;
+        $message = (isset($response->errors)) ? $response->errors[0] : (isset($response->error) ? $response->error : (is_string($response) ? $response : null));
         parent::__construct($message, $code);
     }
 }
